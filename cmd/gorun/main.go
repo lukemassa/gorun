ad08b55e6a56b3f42a981cd3ad6c357dcef23f8c
Luke Massa <lukefrederickmassa@gmail.com>
2025-12-18 15:41:54 -0500

    Cleanup

--- cmd/gorun/main.go
+++ cmd/gorun/main.go
@@ -1,13 +1,68 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 	"syscall"
+	"time"
 
 	log "github.com/lukemassa/clilog"
 	"github.com/lukemassa/gorun/internal/client"
 	"github.com/lukemassa/gorun/internal/config"
 )
+
+func promptYesNo(prompt string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [y/n]: ", prompt)
+
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Warnf("Unexpected error parsing stdin: %v", err)
+			return false
+		}
+
+		switch strings.ToLower(strings.TrimSpace(line)) {
+		case "y", "yes":
+			return true
+		case "n", "no":
+			return false
+		default:
+			fmt.Println("Please answer y or n.")
+		}
+	}
+}
+
+func getExecutable(c *client.Client, mainPackage string, env []string) string {
+	executable, err := c.GetCommand(mainPackage, env)
+	if err != nil {
+		if !errors.Is(err, syscall.ECONNREFUSED) {
+			log.Fatal(err)
+		}
+		log.Warn("Gorun appears to not be running")
+		if !promptYesNo("Start up gorund?") {
+			log.Fatal("Exiting")
+		}
+		cmd := exec.Command("gorund", "start")
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(100 * time.Millisecond)
+		log.Warn("Started up gorun")
+		executable, err = c.GetCommand(mainPackage, env)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	}
+	return executable
+}
 
 func main() {
 	workingDir := os.Getenv("GORUN_WORKING_DIR")
@@ -35,18 +90,13 @@ func main() {
 	switch verb {
 	case "run":
 
-		executable, err := client.GetCommand(mainPackage, env)
-		// URL host is ignored — must be syntactically valid, but irrelevant.
-		if err != nil {
-			log.Fatal(err)
-		}
-
+		executable := getExecutable(client, mainPackage, env)
 		args := []string{executable}
 		args = append(args, mainArgs...)
 
 		log.Debugf("Compiled context for %q to %q, passing additional args %v", mainPackage, executable, mainArgs)
 
-		err = syscall.Exec(executable, args, env)
+		err := syscall.Exec(executable, args, env)
 		if err != nil {
 			log.Fatalf("exec failed: %v", err)
 		}
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 go build -o ~/.local/go-tools/gorun cmd/gorun/main.go
+go build -o ~/.local/go-tools/gorund cmd/gorund/main.go
